f71bee64f27c200f3cde4b44fcf30b074557036e
Chris Chabot <chabotc@gmail.com>
2018-04-27 04:49:48 +0100

    listen

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const token = process.env.SLACK_TOKEN;
 const rtm = new RTMClient(token);
 rtm.start();
 
-const conversationId = 'CA1B2S3AN';
+rtm.on('message', (event) => {
+  if ( (message.subtype && message.subtype === 'bot_message') ||
+       (!message.subtype && message.user === rtm.activeUserId) ) {
+    return;
+  }
+  console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+});
 
+
+const conversationId = 'CA1B2S3AN';
 rtm.sendMessage('Hello there', conversationId)
   .then((res) => {
     // `res` contains information about the posted message
     console.log('Message sent: ', res.ts);
   })
   .catch(console.error);
+
