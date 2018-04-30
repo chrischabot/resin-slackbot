0bd79631b8cb99441c1cd5a086c40cbdbc190f18
Chris Chabot <chabotc@gmail.com>
2018-04-30 03:47:41 +0100

    ..

--- data/index.js
+++ data/index.js
@@ -2,13 +2,46 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const nodeimu = require('nodeimu');
 const {
   exec
 } = require('child_process');
+const TIMEOUT = 1000
 
+var IMU = new nodeimu.IMU()
+let tic = new Date()
 
 server.listen(8080);
 
+function requestData() {
+  setTimeout(() => {
+    tic = new Date()
+    IMU.getValue(getSensorsData)
+  }, TIMEOUT - (new Date() - tic))
+}
+
+function getSensorsData(e, data) {
+  if (e) {
+    return
+  }
+  const measure = createPayload(data)
+  console.log(measure)
+  //emitToSocket(measure)
+  //requestData()
+}
+
+function createPayload(data) {
+  const { timestamp, temperature, pressure, humidity } = data
+
+  return {
+    date: timestamp.toISOString(),
+    temperature,
+    pressure,
+    humidity
+  }
+}
+
+requestData()
 
 let getCpuTemp = function(socket) {
   'use strict';
